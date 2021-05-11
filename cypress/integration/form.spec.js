82011b35f787b964c6ae00d473071cc6a547a4ce
Kelly Young <kkristineyoung@gmail.com>
2021-05-10 21:38:00 -0500

    Added tests for visiting page and filling out form

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -10,5 +10,24 @@ describe("Form test", () => {
     // visit = a cypress method for browsing to a given path
     // get = a method for selecting elements in the page
     cy.get("form");
+    // 'visit' and 'get' are Cypress commands that act as implicit assertions. 
+    // Implicit assertion: if the element is in the page Cypress considers the test passed.
+    cy.get('input[name="name"]')
+      .type("Molly")
+      .should("have.value", "Molly");
+    // 'should' is a cypress command
+    // 'should' creates an assertion and is used to check if an input is updating its state as expected
+    // 'type' is a cypress command
+    // 'type' types the text into the input
+    cy.get('input[name="email"]')
+      .type("molly@dev.dev")
+      .should("have.value", "molly@dev.dev");
+    
+    cy.get("textarea")
+      .type("Mind if I ask you some silly question?")
+      .should("have.value", "Mind if I ask you some silly question?")
+
+    cy.get("form").submit();
+
   });
 });
